5fefb60364144cc909c04ac8caa86322e4e3e5a3
ILister1 <ilister@qa.com>
2020-12-10 15:58:29 +0000

    fixed http triggers

--- Service1Joiner/__init__.py
+++ Service1Joiner/__init__.py
@@ -5,8 +5,10 @@ import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    numbers = requests.get("http://localhost:7071/api/Service2NumGen").text
-    letters = requests.get("http://localhost:7071/api/Service3LetGen").text
-    username = (''.join(numbers.text + letters.text) for i in range(5))
+    numbers = requests.get("https://usernamegenerator.azurewebsites.net/api/service2numgen").text
+    letters = requests.get("https://usernamegenerator.azurewebsites.net/api/service3letgen").text
+   # numbers = "86864"
+   # letters = 'hyvmd'
+    username = numbers + letters
     
     return func.HttpResponse(f"Hello, {username}. This HTTP triggered function executed successfully.")
